cb8d240bf76359336e04ef90b6acc3198d2f6c2b
Vladislav <vladislav.borsch@gmail.com>
2014-11-02 17:35:29 +0200

    Cycles 3 Tickets complete

--- src/com/hometask/task03/CyclesThird.java
+++ src/com/hometask/task03/CyclesThird.java
@@ -6,17 +6,26 @@ package com.hometask.task03;
 public class CyclesThird {
     public static void main(String[] args) {
         int i = 0;
-        int sum1 = 0;
-        int sum2 = 0;
-        for (int ticket = 1; ticket < 999999; ticket++) {
-            i = ticket;
-            i /= 1000;
-            sum1 += (i % 1000);
-            sum2 += i;
+        int j = 0;
 
+        for (int ticket = 1; ticket < 999999; ticket++) {
+            i = ticket / 1000;
+            int sum1 = 0;
+            int sum2 = 0;
+            while (i!=0) {
+                sum1 += (i % 10);
+                i /= 10;
+            }
+            i = ticket % 1000;
+            while (i!=0) {
+                sum2 += (i % 10);
+                i /= 10;
+            }
             if (sum2 == sum1){
-            System.out.println(ticket);
+            j++;
+                System.out.println("Lucky ticket №: " + j + " is " + ticket);
             }
         }
+        System.out.println("There are " + j + " lucky tickets in one roll.");
     }
 }
