65b75b8b2a3f18697c0168946eba0174e21d2eb0
VladShapeshifter <vladislav.borsch@gmail.com>
2014-11-05 22:38:07 +0200

    School - Student class.

---
+++ src/com/hometask/task04/Student.java
@@ -0,0 +1,21 @@
+package com.hometask.task04;
+
+/**
+ * Created by bvl on 11/5/2014.
+ */
+public class Student extends Person {
+    private String idNumber;
+    private double gpa;
+    public void setIdNumber(String idnumber){
+        this.idNumber = idnumber;
+    }
+    public String getIdNumber (){
+        return idNumber;
+    }
+    public void setGpa(double gpa){
+        this.gpa = gpa;
+    }
+    public double getGpa (){
+        return gpa;
+    }
+}
