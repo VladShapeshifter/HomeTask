c661ca2e5b8bc46b501ef89016ef8273f0ea1921
VladShapeshifter <vladislav.borsch@gmail.com>
2014-11-05 22:36:55 +0200

    School - Person class.

---
+++ src/com/hometask/task04/Person.java
@@ -0,0 +1,29 @@
+package com.hometask.task04;
+
+/**
+ * Created by bvl on 11/5/2014.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName (){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge (){
+        return age;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    public String getGender (){
+        return gender;
+    }
+
+}
