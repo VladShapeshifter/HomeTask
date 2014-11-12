e88f8255245f664b7de7b38d5c280fe52a7e37a7
VladShapeshifter <vladislav.borsch@gmail.com>
2014-11-12 21:54:21 +0200

    Task05 Test Class01

---
+++ src/com/hometask/task05/Test.java
@@ -0,0 +1,11 @@
+package com.hometask.task05;
+
+/**
+ * Created by bvl on 11/12/2014.
+ */
+public class Test {
+    public static void main(String[] args) {
+        FractionNumberOperationImp testOne = new FractionNumberOperationImp();
+
+    }
+}
