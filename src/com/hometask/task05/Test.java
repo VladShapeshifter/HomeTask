684ad6594739e3e2b7a1014200c728b3d4a59723
Vladislav <vladislav.borsch@gmail.com>
2014-11-15 22:24:16 +0200

    task05 Test.class 01

--- src/com/hometask/task05/Test.java
+++ src/com/hometask/task05/Test.java
@@ -1,11 +1,19 @@
 package com.hometask.task05;
 
+import com.sourceit.hometask.basic.FractionNumber;
+
 /**
+<<<<<<< Updated upstream
  * Created by bvl on 11/12/2014.
  */
 public class Test {
     public static void main(String[] args) {
-        FractionNumberOperationImp testOne = new FractionNumberOperationImp();
+        FractionNumberOperationImpl fractionNumberWork = new FractionNumberOperationImpl();
+        FractionNumberOperationImpl fractionNumber1 = new FractionNumberOperationImpl();
+        FractionNumberOperationImpl fractionNumber2 = new FractionNumberOperationImpl();
+        FractionNumber C;
+        C = fractionNumberWork.add(fractionNumber1.parseFractionNumber("1/4"), fractionNumber2.parseFractionNumber("2/4"));
+        System.out.println("Sum = " + C.doubleValue());
 
     }
 }
