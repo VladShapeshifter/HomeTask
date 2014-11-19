cbc6867afb9f6bd6804cc190634dbebcaf91f8f0
VladShapeshifter <vladislav.borsch@gmail.com>
2014-11-19 22:36:43 +0200

    Task06StringUtilsImpl Class02

--- src/com/hometask/task06/StringUtilsImpl.java
+++ src/com/hometask/task06/StringUtilsImpl.java
@@ -17,47 +17,34 @@ public class StringUtilsImpl implements StringUtils {
         result = Math.floor(nDivinder / nDivisor);
         return result;
     }
-
     public int[] findWord(String var1, String var2) throws NullPointerException {
         int[] arrayIndex = new int[3];
         if (var1.contains(var2)) {
             String[] sArrS = var1.split("\\s+");
-//            System.out.println(Arrays.toString(sArrS));
-//            arrayIndex[0] = Integer.parseInt(sArrS[0]);
-//            arrayIndex[1] = Integer.parseInt(sArrS[1]);
-//            arrayIndex[2] = Integer.parseInt(sArrS[2]);
-//            arrayIndex[3] = Integer.parseInt(sArrS[3]);
-//            arrayIndex[4] = Integer.parseInt(sArrS[4]);
             for (int i = 0, j = 0; i < sArrS.length; i++, j++) {
                 if (sArrS[i].equals(var2)) {
                     arrayIndex[j] = i;
-//                    System.out.println(i);
                 }
             }
         }
         return arrayIndex;
     }
-    public double[] findNumbers(String var1) throws CustomException {
+    public double[] findNumbers(String var1) throws CustomException /*, NumberFormatException*/ {
         double[] arrayD = new double[2];
         if (var1.contains(".")) {
             String[] sArrN = var1.split("\\s+");
-//            arrayD[0] = Double.parseDouble(sArrN[0]);
-//            arrayD[1] = Double.parseDouble(sArrN[1]);
             for (int i = 0, j = 0; i < sArrN.length; i++, j++) {
-//                if (sArrN[i].equals(var2)) {
-                    double number = Double.parseDouble(sArrN[i]);
-                    arrayD[j] = number;
-//                    System.out.println(i);
-//                }
+                double number = Double.parseDouble(sArrN[i]);
+                /*throw new NumberFormatException("Use double value instead String.");*/
+                arrayD[j] = number;
             }
-        } /*else throw new CustomException("Not double value was found in the text");*/
+        } else throw new CustomException("Not double value was found in the text.");
         return arrayD;
     }
-
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         StringUtilsImpl obj = new StringUtilsImpl();
         System.out.println(obj.div("7", "4"));
         System.out.println(Arrays.toString(obj.findWord("go gogo go", "go")));
         System.out.println(Arrays.toString(obj.findNumbers("12.5 13.6")));
-    }
+    }*/
 }
