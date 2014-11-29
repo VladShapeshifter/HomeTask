9aaace2948f3c40db0e0adf60353a90055854239
VladShapeshifter <vladislav.borsch@gmail.com>
2014-11-29 18:58:19 +0200

    task07 SetUtilsImpl Class02

--- src/com/hometask/task07/SetUtilsImpl.java
+++ src/com/hometask/task07/SetUtilsImpl.java
@@ -2,9 +2,7 @@ package com.hometask.task07;
 
 import com.sourceit.hometask.collections.SetUtils;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 /**
  * Created by Vladislav on 24.11.2014.
@@ -12,12 +10,26 @@ import java.util.SortedSet;
 public class SetUtilsImpl implements SetUtils {
 
     public SortedSet<String> orderedSet(Collection<Integer> var1, Set<String> var2) throws NullPointerException {
-
+        ArrayList<Integer> orderedNumbers = new ArrayList<Integer>();
+        List<String> nonOrderedLetters = new ArrayList<String>();
+        Set<String> set = new LinkedHashSet<String>();
+        if (orderedNumbers.size() == nonOrderedLetters.size()) {
+            Collections.sort(nonOrderedLetters, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return 0;
+                }
+            });
+        }
         return null;
     }
 
     public Set<Integer> customOrderSet(Integer... var1) throws NullPointerException {
-        return null;
+        Set<Integer> integerSet = new LinkedHashSet<Integer>();
+        for (Integer temp : var1){
+            integerSet.add(temp);
+        }
+        return integerSet;
     }
 
 }
