dcedcf03e788f5977145f8bfe28208d6a7392e33
Vladislav <vladislav.borsch@gmail.com>
2014-11-29 14:35:03 +0200

    task07 SetUtilsImpl Class 03

--- src/com/hometask/task07/SetUtilsImpl.java
+++ src/com/hometask/task07/SetUtilsImpl.java
@@ -2,41 +2,22 @@ package com.hometask.task07;
 
 import com.sourceit.hometask.collections.SetUtils;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Set;
+import java.util.SortedSet;
 
 /**
  * Created by Vladislav on 24.11.2014.
  */
-public class SetUtilsImpl implements SetUtils{
+public class SetUtilsImpl implements SetUtils {
 
-    public SortedSet<String> orderedSet(Collection<Integer> var1, Set<String> var2) throws NullPointerException{
+    public SortedSet<String> orderedSet(Collection<Integer> var1, Set<String> var2) throws NullPointerException {
 
-/*        Collection<Integer> integerCollection = new ArrayList<Integer>();
-        for (Integer temp : var1){
-            integerCollection.add(temp);
-        }
-        Set<String> stringSet = new TreeSet<String>();
-        for (String temp : var2){
-            stringSet.add(temp);
-        }*/
-
-        SortedSet<String> mySort = new TreeSet<String>();
-
-        if (var1.size() == var2.size()) {
-            mySort.addAll(var2);
-            mySort.comparator(var2,);
-//            mySort.iterator();
-
-
-        }
-        return mySort;
+        return null;
     }
 
-    public Set<Integer> customOrderSet(Integer... var1) throws NullPointerException{
-        Set<Integer> integerSet = new LinkedHashSet<Integer>();
-        for (Integer temp : var1){
-            integerSet.add(temp);
-        }
-        return integerSet;
+    public Set<Integer> customOrderSet(Integer... var1) throws NullPointerException {
+        return null;
     }
+
 }
