2789190f68eb980a22481ffbfe20962e694b4747
VladShapeshifter <vladislav.borsch@gmail.com>
2014-12-02 22:13:21 +0200

    task08 CopyFileStrategyImpl Class01

---
+++ src/com/hometask/task08/CopyFileStrategyImpl.java
@@ -0,0 +1,20 @@
+package com.hometask.task08;
+
+import com.sourceit.hometask.io.CopyFileStrategy;
+import com.sourceit.hometask.io.FileAlreadyPresentsException;
+import com.sourceit.hometask.io.FileCopyFailedException;
+
+import java.io.File;
+
+/**
+ * Created by bvl on 12/2/2014.
+ */
+public class CopyFileStrategyImpl implements CopyFileStrategy {
+    public void copyFile(String var1, String var2) throws FileAlreadyPresentsException, FileCopyFailedException {
+
+    }
+
+    public void copyFile(File var1, File var2) throws FileAlreadyPresentsException, FileCopyFailedException {
+
+    }
+}
