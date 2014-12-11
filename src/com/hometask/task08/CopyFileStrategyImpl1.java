934c24352f0a9c3bab09daa6a0ef58216ab37cdc
Vladislav <vladislav.borsch@gmail.com>
2014-12-11 22:34:43 +0200

    Bug fixed and simplified (with commented old code)

--- src/com/hometask/task08/CopyFileStrategyImpl1.java
+++ src/com/hometask/task08/CopyFileStrategyImpl1.java
@@ -12,29 +12,22 @@ import java.io.*;
  */
 public class CopyFileStrategyImpl1 implements CopyFileStrategy {
     public void copyFile(String var1, String var2) throws FileAlreadyPresentsException, FileCopyFailedException {
-        FileOutputStream os = null;
-        try (FileInputStream is = new FileInputStream(var1)){
-            os = new FileOutputStream(var2);
-            byte[] buffer = new byte[4096];
-            is.read(buffer);
-            os.write(buffer);
+        try (FileInputStream is = new FileInputStream(var1);
+             FileOutputStream os = new FileOutputStream(var2)){
+            int buf;
+            while ((buf = is.read()) != -1) {
+                os.write(buf);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (os != null) {
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
     public void copyFile(File var1, File var2) throws FileAlreadyPresentsException, FileCopyFailedException {
-        FileOutputStream os = null;
+        copyFile(var1.getAbsolutePath(), var2.getAbsolutePath());
+        /*FileOutputStream os = null;
         try (FileInputStream is = new FileInputStream(var1)) {
             os = new FileOutputStream(var2);
             byte[] buffer = new byte[4096];
@@ -52,6 +45,6 @@ public class CopyFileStrategyImpl1 implements CopyFileStrategy {
                     e.printStackTrace();
                 }
             }
-        }
+        }*/
     }
 }
