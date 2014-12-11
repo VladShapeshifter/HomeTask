ff28a70d6176dd07523e999dc90ae3877f77b122
Vladislav <vladislav.borsch@gmail.com>
2014-12-11 18:38:19 +0200

    First version

---
+++ src/com/hometask/task08/CopyFileStrategyImpl1.java
@@ -0,0 +1,57 @@
+package com.hometask.task08;
+
+import com.sourceit.hometask.io.CopyFileStrategy;
+import com.sourceit.hometask.io.FileAlreadyPresentsException;
+import com.sourceit.hometask.io.FileCopyFailedException;
+import com.sun.org.apache.bcel.internal.classfile.SourceFile;
+
+import java.io.*;
+
+/**
+ * Created by bvl on 12/2/2014.
+ */
+public class CopyFileStrategyImpl1 implements CopyFileStrategy {
+    public void copyFile(String var1, String var2) throws FileAlreadyPresentsException, FileCopyFailedException {
+        FileOutputStream os = null;
+        try (FileInputStream is = new FileInputStream(var1)){
+            os = new FileOutputStream(var2);
+            byte[] buffer = new byte[4096];
+            is.read(buffer);
+            os.write(buffer);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void copyFile(File var1, File var2) throws FileAlreadyPresentsException, FileCopyFailedException {
+        FileOutputStream os = null;
+        try (FileInputStream is = new FileInputStream(var1)) {
+            os = new FileOutputStream(var2);
+            byte[] buffer = new byte[4096];
+            is.read(buffer);
+            os.write(buffer);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
