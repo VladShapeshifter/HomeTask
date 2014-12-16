16cc031fb8badc6b2d4a11425daa92a5edf8d5e7
Vladislav <vladislav.borsch@gmail.com>
2014-12-16 21:20:29 +0200

    add realisation for method setFileNameSearchString()

--- src/com/hometask/task09/FindFilesTaskImpl.java
+++ src/com/hometask/task09/FindFilesTaskImpl.java
@@ -3,8 +3,11 @@ package com.hometask.task09;
 import com.sourceit.hometask.threads.FindFilesTask;
 import com.sourceit.hometask.threads.TaskExecutionFailedException;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 
 /**
@@ -15,6 +18,7 @@ public class FindFilesTaskImpl implements FindFilesTask {
     private String fileNameSearchString;
     private PrintStream ps;
     private int tryCount;
+
     @Override
     public void setDirectory(String s) throws NullPointerException, FileNotFoundException {
         directory = s;
@@ -22,7 +26,29 @@ public class FindFilesTaskImpl implements FindFilesTask {
 
     @Override
     public void setFileNameSearchString(String s) throws IllegalArgumentException {
-        fileNameSearchString = s;
+        File file = new File(directory);
+        // Find all files in the directory
+        if (s.contains("*")) {
+            String[] listOfAll1 = file.list();
+            for (String printAll : listOfAll1) {
+                System.out.println(printAll);
+            }
+        }
+        // Find files with one character file name
+        if (s.contains("?")) {
+            String[] listOfOne1 = file.list();
+            ArrayList arrayListOne = new ArrayList();
+            for (String printOne1 : listOfOne1) {
+                if (printOne1.length() == 1) {
+                    arrayListOne.add(printOne1);
+                }
+            }
+            String[] listOfOne2 = new String[arrayListOne.size()];
+            arrayListOne.toArray(listOfOne2);
+            for (String printOne2 : listOfOne2) {
+                System.out.println(printOne2);
+            }
+        }
     }
 
     @Override
