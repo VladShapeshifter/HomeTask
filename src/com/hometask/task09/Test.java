1e1ed444f231853f3c577bedcc9699975e0042ae
Vladislav <vladislav.borsch@gmail.com>
2014-12-16 12:32:40 +0200

    add path's for copyFileTask object.

--- src/com/hometask/task09/Test.java
+++ src/com/hometask/task09/Test.java
@@ -1,8 +1,16 @@
 package com.hometask.task09;
 
+import com.hometask.task08.CopyFileFactoryImpl;
+
 /**
  * Created by Vladislav on 14.12.2014.
  */
 public class Test {
-
+    public static void main(String[] args) {
+        CopyFileTaskImpl copyFileTask = new CopyFileTaskImpl();
+        copyFileTask.setSourceFilePath("path2/file1.txt");
+        copyFileTask.setDestinyFilePath("path2/file1copy.txt");
+        CopyFileFactoryImpl copyFileFactory= new CopyFileFactoryImpl();
+        copyFileFactory.createBufferedCopyFileStrategy().copyFile();
+    }
 }
